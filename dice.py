bcdd9e8d22b71a25c2cc205d3ad4de0e31d1a1da
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-02 23:57:22 +0100

    Add a default six-sided die

--- dice.py
+++ dice.py
@@ -7,3 +7,7 @@ class Die:
         if not isinstance (sides, int):
             raise ValueError("Sides must be a whole number")
         self.value = value or random.randint(1, sides)
+
+class D6(Die):
+    def __init__(self, value=0):
+        super().__init__(sides=6, value=value)
